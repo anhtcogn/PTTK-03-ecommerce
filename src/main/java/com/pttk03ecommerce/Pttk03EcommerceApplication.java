b2c20dc0baf36fdbfc3ef9c4a3bc3298be2a3148
anhtcogn <anhtcogn@gmail.com>
2022-11-16 17:06:44 +0700

    Initial commit

---
+++ src/main/java/com/pttk03ecommerce/Pttk03EcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.pttk03ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pttk03EcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pttk03EcommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pttk03ecommerce/Pttk03EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pttk03ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pttk03EcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
